acc018751e997660c81fd01cbac6621a95fdf8c5
KrishnaPrabha13 <86238794+KrishnaPrabha13@users.noreply.github.com>
2021-06-25 14:57:06 +0530

    Delete prime.js

--- prime.js
+++
@@ -1,9 +0,0 @@
-var a = [2,3,5,6,7,8,9,11];
-a = a.filter((num) => {
-    for(var i=2; i<= Math.sqrt(num);i++){
-        if(num %i ==0)
-        return false;
-    }
-    return true;
-});
-console.log(a);
