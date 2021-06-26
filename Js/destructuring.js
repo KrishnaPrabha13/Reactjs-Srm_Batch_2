9ddab316c05b98145d8e817b08e338d2879ed0f8
KrishnaPrabha13 <86238794+KrishnaPrabha13@users.noreply.github.com>
2021-06-26 23:19:17 +0530

    Add files via upload

---
+++ Js/destructuring.js
@@ -0,0 +1,19 @@
+let x,y,z;
+
+let data = [10,20,30,40,50,60,70];
+
+let student =[{id:1, name:"priya",course:"js"},
+              {id:2, name:"Ram",course:"Javascript"}
+            ];
+
+[x,y,z] =data;
+
+console.log(x);
+console.log(y);
+console.log(z);
+
+
+let a,b;
+[a,b] = student;
+console.log(typeof(a));
+console.log(a);
---
+++ Js/equaloperator.js
@@ -0,0 +1,21 @@
+var a = 500;
+if(a == 500) //compare value irrespective of datatype
+{
+    console.log("Both are equal");
+}
+else{
+    console.log("Not equal");
+}
+
+console.log(a);
+
+var a1 = "100";
+if(a === 100) //compare value with datatype as well 
+{
+    console.log("Both are equal");
+}
+else{
+    console.log("Not equal");
+}
+
+console.log(a1);
