e8c7fddc902ae18f8442ce2f28a61ac769554961
KrishnaPrabha13 <86238794+KrishnaPrabha13@users.noreply.github.com>
2021-07-01 13:01:48 +0530

    Add files via upload

---
+++ React/Ex2_Lable/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import MyLable from './component/MyLable';
+
+
+function App() {
+  return (
+    <div className="App">
+      <MyLable/>
+    </div>
+  );
+}
+
+export default App;
