e56e5d1ea7be61a65a5e35dc86612962fcd2b1df
KrishnaPrabha13 <86238794+KrishnaPrabha13@users.noreply.github.com>
2021-07-08 10:51:38 +0530

    Add files via upload

---
+++ React/Week3/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import ComponentA from './Components/ComponentA';
+import ComponentB from './Components/ComponentB';
+function App() {
+  return (
+    <div className="App">
+      <ComponentA/>
+      <hr/>
+      <ComponentB/>
+    </div>
+  );
+}
+
+export default App;
